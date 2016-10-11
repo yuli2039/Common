f43bba4db6d5cfe5db505fc0c2e7753a042c0790
SD-201608261143\Administrator <229868243@qq.com>
2016-10-11 10:42:25 +0800

    z

--- mvpfw/src/main/java/com/yu/retrofittest/rx/DefaultTransformer.java
+++ mvpfw/src/main/java/com/yu/retrofittest/rx/DefaultTransformer.java
@@ -18,15 +18,27 @@ public class DefaultTransformer<T> implements Observable.Transformer<T, T> {
         return tObservable.subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .map(new Func1<T, T>() {// 通用错误处理，判断code,也可以用自定义converter来预处理异常
+//                .map(new Func1<T, T>() {// 通用错误处理，判断code,也可以用自定义converter来预处理异常
+//                    @Override
+//                    public T call(T t) {
+//                        if (t instanceof BaseEntity) {
+//                            BaseEntity e = (BaseEntity) t;
+//                            int code = e.getCode();
+//                            if (code != 200) throw new ApiException(code, e.getMessage());
+//                        }
+//                        return t;
+//                    }
+//                })
+                .flatMap(new Func1<T, Observable<T>>() {
                     @Override
-                    public T call(T t) {
+                    public Observable<T> call(T t) {
                         if (t instanceof BaseEntity) {
                             BaseEntity e = (BaseEntity) t;
                             int code = e.getCode();
-                            if (code != 200) throw new ApiException(code, e.getMessage());
+                            if (code != 200)
+                                return Observable.error(new ApiException(code, e.getMessage()));
                         }
-                        return t;
+                        return Observable.just(t);
                     }
                 });
     }
